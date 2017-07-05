fa35e053310494df9819b58c13429e42aa4bcd78
bmartel <bmartel@192.168.254.88>
2017-07-05 11:28:37 +0200

    Expects Algolia credentials from config.json

--- scripts/data_index.js
+++ scripts/data_index.js
@@ -7,11 +7,26 @@
  */
 
 var restaurants_merge_path = __dirname + '/../resources/dataset/restaurants_merge.json';
-
 var algoliasearch = require('algoliasearch');
+var fs = require('fs');
 
-var client = algoliasearch('AQ7L99YGHS', '2cab74de88406b704912e610ff2da5e6', {
-  // timeout indexing error if left to default (is timeout correlated to indexing time / #objects) ???
+fs.stat('config.json', function(err, stat) {
+  if (err != null) {
+    if (err.code == 'ENOENT') {
+    // file does not exist
+    console.log('config.json does not exist. Please create one with both "applicationID" and "apiKey" defined');
+    }
+    else {
+    console.log('Error: ', err.code);
+    }
+  }
+});
+var config = require('./config.json');
+
+//var client = algoliasearch('AQ7L99YGHS', '2cab74de88406b704912e610ff2da5e6', {
+var client = algoliasearch(config.applicationID, config.apiKey, {
+  // timeout indexing error if left to default (is timeout correlated to
+  // indexing time / #objects) ???
   timeout : 30000
 });
 
@@ -20,8 +35,9 @@ var contactsJSON = require(restaurants_merge_path);
 
 index.addObjects(contactsJSON, function(err, content) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log("Restaurants Data Indexing Complete")
+    console.log("Restaurants Data Indexing Complete");
   }
 });
+
